f653de3c5f1d1c621b8a937ba963ea175afa1ffb
FeyzanurOlcay <nurolcay000@gmail.com>
2025-05-18 00:10:57 +0300

    Initial Commit

---
+++ MvcProject/MvcProject/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcProject.Models;
+using System.Collections.Generic;
+
+namespace MvcProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        public static List<Product> products = new List<Product>
+        {
+                new Product { Id = 1,Name="Laptop",Price=15000},
+                new Product { Id = 2,Name="Telefon",Price=9000},
+                new Product { Id = 3,Name="Tablet",Price=4000}
+        };
+
+        public IActionResult List()
+        {
+            return View(products);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(product);
+            }
+            product.Id = products.Count+1;
+            products.Add(product);
+            return RedirectToAction("List");
+        }
+
+
+    }
+}
---
+++ MvcProject/MvcProject/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcProject.Models
+{
+	public class Product
+	{
+		public int Id { get; set; }
+		[Required(ErrorMessage ="Ürün adı zorunludur!")]
+		public string Name { get; set; }
+		[Range(1,10000,ErrorMessage ="Fiyat 1 ve 10.000 aralığında olmalıdır")]
+		public decimal Price { get; set; }
+	}
+}
