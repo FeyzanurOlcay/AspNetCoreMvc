006a22d22e97a9583560ed6d0ff42c75bee99e6f
FeyzanurOlcay <nurolcay000@gmail.com>
2025-05-17 23:28:47 +0300

    Initial Commit

---
+++ AntiForgeryTokens/AntiForgeryTokens/Controllers/CommentsController.cs
@@ -0,0 +1,31 @@
+﻿using AntiForgeryTokens.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AntiForgeryTokens.Controllers
+{
+	public class CommentsController : Controller
+	{
+		private static List<Comment> comments = new();
+
+		public IActionResult Index()
+		{
+			return View(comments);
+		}
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		// POST: /Comments/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken] // Bu satır CSRF koruması sağlar
+		public IActionResult Create(Comment comment)
+		{
+			if (!ModelState.IsValid)
+				return View(comment);
+			comment.Id = comments.Count + 1;
+			comments.Add(comment);
+			return RedirectToAction("Index");
+		}
+	}
+}
---
+++ AntiForgeryTokens/AntiForgeryTokens/Models/Comment.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AntiForgeryTokens.Models
+{
+	public class Comment
+	{
+		public int Id { get; set; }
+		[Required(ErrorMessage = "Yorum boş olamaz")]
+		public string Message { get; set; }
+	}
+}
