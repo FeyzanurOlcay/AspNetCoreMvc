cc09dab31d11ab76483af7f9902528b0475d393a
FeyzanurOlcay <nurolcay000@gmail.com>
2025-05-17 22:24:47 +0300

    Initial Commit

---
+++ UrunProje/UrunProje/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UrunProje.Models;
+
+namespace UrunProje.Controllers
+{
+    public class ProductController : Controller
+    {
+        private static List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, Name ="Laptop",Price = 5000},
+            new Product { Id = 2, Name ="Tablet",Price = 1000},
+            new Product { Id = 3, Name ="Telefon",Price = 8000}
+        };
+
+        public IActionResult List()
+        {
+            return View(products);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(product);
+            }
+            product.Id=products.Count+1;
+            products.Add(product);
+            return RedirectToAction("List");
+        }
+    }   
+}
---
+++ UrunProje/UrunProje/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UrunProje.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Ürün adı zorunludur")]
+        public string Name { get; set; }
+
+        [Range(1,10000,ErrorMessage ="Fiyat 1 ile 10.000 arasında olmalıdır")]
+
+        public decimal Price { get; set; }
+    }
+}
