b64aa94fc4a878b84025f9e28db2bcc98cd1cdfc
FeyzanurOlcay <nurolcay000@gmail.com>
2025-05-23 12:40:04 +0300

    Initial commit

---
+++ WebApplication1/WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+	public class ProductController : Controller
+	{
+		public static List<Products> products = new List<Products>();
+
+		public IActionResult Index()
+		{
+			return View(products);
+		}
+		public IActionResult Create()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Create(Products product)
+		{
+			if(!ModelState.IsValid)
+			{
+				return View(products);
+			}
+			product.Id = products.Count + 1;
+			products.Add(product);
+			return RedirectToAction("Index");
+		}
+	}
+}
---
+++ WebApplication1/WebApplication1/Models/Products.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace WebApplication1.Models
+{
+	public class Products
+	{
+		public int Id { get; set; }
+		[Required(ErrorMessage ="Ürün adı zorunludur.")]
+		public string Name { get; set; }
+		[Range(1,10000,ErrorMessage ="Fiyat 1 ve 10000 aralığında olmalıdır.")]
+		public decimal Price { get; set; }
+	}
+}
